9b805feb69d12fb1b9650b13e3436d92bffa1341
made-edina <118876413+made-edina@users.noreply.github.com>
2022-12-05 22:40:18 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,20 +5,12 @@ import Blog from './components/Blog';
 import data from './data';
 
 function App() {
-  const blog = data.map(item => {
-    return (
-      <Blog 
-        key={item.title}
-        {...item}
-      />
-    )
-  })
   
   return (
     <div>
       <Header />
       <main>
-      {blog}
+      {data.map(item => <Blog key={item.title} {...item}/>)}
       </main>
       
     </div>
